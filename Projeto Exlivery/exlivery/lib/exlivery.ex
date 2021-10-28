49a43541f13b8d9eba346e806f561a61510baf6b
Leandro Moreira <leandro.moreira07@gmail.com>
2021-10-27 23:11:58 -0300

    Facilitando a criação do User

--- Projeto Exlivery/exlivery/lib/exlivery.ex
+++ Projeto Exlivery/exlivery/lib/exlivery.ex
@@ -1,6 +1,12 @@
 defmodule Exlivery do
 
-  def hello do
-    :world
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate 
+
+  def start_agents do
+   UserAgent.start_link(%{})
   end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+ 
 end
